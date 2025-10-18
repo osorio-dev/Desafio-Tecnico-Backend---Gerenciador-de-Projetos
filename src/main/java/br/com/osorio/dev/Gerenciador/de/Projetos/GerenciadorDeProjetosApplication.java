c76fd097062458deb202cd009f831ef5e3a3f293
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-10-18 12:45:56 -0300

    projeto inicial

---
+++ src/main/java/br/com/osorio/dev/Gerenciador/de/Projetos/GerenciadorDeProjetosApplication.java
@@ -0,0 +1,13 @@
+package br.com.osorio.dev.Gerenciador.de.Projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/osorio/dev/Gerenciador/de/Projetos/GerenciadorDeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.osorio.dev.Gerenciador.de.Projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
